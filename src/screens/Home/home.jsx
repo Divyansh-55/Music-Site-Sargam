c3c0d9283011f3db1c376b74ea6543ddd37e94dc
Divyansh <divyanshgupta0103@gmail.com>
2024-03-03 14:51:48 +0530

    Cretaing Music Player

--- src/screens/Home/home.jsx
+++ src/screens/Home/home.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Feed from '../feed';
-import Trending from '../trending';
-import Library from '../library';
-import Favorites from '../favourites';
-import Player from '../player';
+import Feed from '../Feed/feed';
+import Trending from '../Trending/trending';
+import Library from '../Library/library';
+import Favorites from '../favourite/favourites';
+import Player from '../Player/player';
 import './home.css';
+import Sidebar from '../../Components/Sidebar/index';
 
 export default function Home() {
   return (
     <BrowserRouter>
       <div className='main-body'>
+        <Sidebar/>
         <Routes>
           <Route path='/library' element={<Library />} />
           <Route path="/feed" element={<Feed />} />
